af82c63d8c1fd95b5de57f020980dcc1cd539fe0
Aarya Reddy <aaryareddy@aaryas-mbp.mynetworksettings.com>
2024-09-28 15:07:48 -0700

    Skeleton Code

---
+++ PlayerClient.h
@@ -0,0 +1,8 @@
+#ifndef PLAYER_CLIENT_H
+#define PLAYER_CLIENT_H
+
+class PlayerClient {
+
+};
+
+#endif // PLAYER_CLIENT_H
---
+++ TrackerServer.h
@@ -0,0 +1,12 @@
+#ifndef TRACKER_SERVER_H
+#define TRACKER_SERVER_H
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class TrackerServer {
+
+};
+
+#endif // TRACKER_SERVER_H
